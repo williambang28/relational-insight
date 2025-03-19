96293d382ae850dc15bcdfd5ef1b65206c56222e
CoolGuyOn <williambang24@icloud.com>
2025-03-19 16:43:08 +0000

    edit packages, add example chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-'use client';
+"use client";
+
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
 
 const Page = () => {
-    
+    return <div style={{ position: "relative", height: "500px" }}>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "just now",
+                sender: "Joe",
+                direction: "incoming",
+                position: "single",
+              }}
+            />
+          </MessageList>
+          <MessageInput placeholder="Type message here" />
+        </ChatContainer>
+      </MainContainer>
+    </div>;
 };
 
 export default Page;
